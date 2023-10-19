d5cf47eea8d7989497d04b3d1301b4b3bbab28fb
Chipss <masonkensinger11@gmail.com>
2023-10-19 14:22:38 -0500

    Update List.java

--- List.java
+++ List.java
@@ -1,3 +1,14 @@
+import java.util.ArrayList;
+
 public abstract class List {
-    
+    abstract void deleteList();
+    abstract boolean saveList();
+
+    private int id = 0;
+    protected String name = "";
+    public ArrayList<Item> items = new ArrayList<Item>();
+
+    public int getID() {
+        return this.id;
+    }
 }
