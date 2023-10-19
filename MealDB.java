3c6a156af16718604e7ae1a302646bce8dc7c710
Chris Blicharz <chblicharz01@gmail.com>
2023-10-19 15:03:35 -0500

    API connection

--- MealDB.java
+++ MealDB.java
@@ -1,20 +1,30 @@
+import java.io.IOException;
+import java.net.URL;
+
 public class MealDB extends API{
-    
     //constructor
     public MealDB()
     {
-
     }
 
     //connect
     public void connect()
     {
-        
+        System.out.println("MealDB has been connected to...");
     }
 
     //query API by recipe name
     public Recipe queryByName(String name)
     {
+        try{
+            String query = "https://www.themealdb.com/api/json/v1/1/search.php?s="+ name;
+            URL url = new URL(query);
+            System.out.println(url);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
         return new Recipe();
     }
 
@@ -33,6 +43,7 @@ public class MealDB extends API{
     //Main
     public static void main(String[] args)
     {
-        
+        MealDB myConnection = new MealDB();
+        myConnection.queryByName("Spaghetti");
     }
 }
