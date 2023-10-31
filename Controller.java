450c5639f90ce834962ba5b7ad63088bb279a0f1
Chipss <masonkensinger11@gmail.com>
2023-10-31 16:17:14 -0500

    Add general definitions for Controller.java

--- Controller.java
+++ Controller.java
@@ -1,5 +1,30 @@
+import java.util.Date;
+
 public class Controller {
     public static void main(String[] args) {
         System.out.println("Test");
+    }
+
+    int numKitchens = 0;
+    int numCarts = 0;
+
+    public void deletePantry(int id) {
+
+    }
+
+    public void createPantry(int id) {
+
+    }
+
+    public void createCart() {
+
+    }
+
+    public void deleteCart(int id) {
+
+    }
+
+    public void addItem(String name, int quantity, Date expirationDate) {
+
     }
 }
