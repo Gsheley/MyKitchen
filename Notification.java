4fb2113ed40c27a8de0a56a2688b51ba3fa86f04
BennyGenny <117399625+BennyGenny@users.noreply.github.com>
2023-10-31 16:31:32 -0500

    Update Notification.java

--- Notification.java
+++ Notification.java
@@ -1,8 +1,19 @@
+import java.util.Date;
+
 public class Notification {
     public String message;
-    public String date;
+    public Date date;
 
     public void addNotification() {
         
     }
+
+    public void editNotification() {
+
+    }
+
+    public void deleteNotification() {
+        
+    }
+
 }
