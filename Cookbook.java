9d8b812ae32973c8c50e0d129e4ba3e9b63672e7
BennyGenny <117399625+BennyGenny@users.noreply.github.com>
2023-11-14 15:13:30 -0600

    cookbook v.1

--- Cookbook.java
+++ Cookbook.java
@@ -1,3 +1,5 @@
 public class Cookbook {
-    
+    protected String cookbookName = "";
+    //Recipe object containing multiple recipes(recipeName: recipeID: recipeIngredients: recipeSteps)
+    public List<Recipe> recipes = new List<Recipe>();
 }
--- CookbookService.java
+++ CookbookService.java
@@ -1,3 +1,6 @@
 public class CookbookService {
-    
+    saveRecipe(newRecipe) {
+        //stores the new recipe object in cookbooks recipes
+        Cookbook.recipes.add(newRecipe);
+    }
 }
