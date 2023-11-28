9c7e6065ba5b967cd52373a9612f2915350dc568
Mason <masonkensinger11@gmail.com>
2023-11-28 14:10:07 -0600

    Work on NotificationService

--- NotificationService.java
+++ NotificationService.java
@@ -1,14 +1,24 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class NotificationService {
-    ArrayList<Notification> notificationList = new ArrayList<Notification>();
+    private ArrayList<Notification> notificationList = new ArrayList<Notification>();
+    private Iterator<Notification> iterator = notificationList.iterator();
+    private int currentNotifID = 1;
 
     public ArrayList<Notification> getNotificationList() {
         return notificationList;
     }
 
     public boolean checkForNotifications() {
-        return true;
+        if (notificationList.isEmpty()) {
+            return false;
+        } else {
+            while (iterator.hasNext()) {
+                
+                return true;
+            }
+        }
     }
 
     public void addNotification() {
@@ -19,7 +29,12 @@ public class NotificationService {
 
     }
 
-    public void deleteNotification() {
-        
+    public void deleteNotification(int idToRemove) {
+        while (iterator.hasNext()) {
+            Notification notif = iterator.next();
+            if (notif.getNotifID() == idToRemove) {
+                iterator.remove(); // Removes the object that matches the notif ID to remove
+            }
+        }
     }
 }
