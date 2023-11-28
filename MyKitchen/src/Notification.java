b8e29eebf1d662c2bb047cb3c8410c8b64fb0a37
Mason <masonkensinger11@gmail.com>
2023-11-28 16:44:51 -0600

    Notification functionality almost complete

--- MyKitchen/src/Notification.java
+++ MyKitchen/src/Notification.java
@@ -1,14 +1,30 @@
-import java.util.Date;
+import java.util.Calendar;
 
 public abstract class Notification {
     private int notifID = 0;
-    private Date notificationDate = null;
+    private Calendar notifDate = Calendar.getInstance();
+
+    Notification(int notifID, Calendar notifDate) {
+        this.notifID = notifID;
+        this.notifDate = notifDate;
+    }
 
     public int getNotifID() {
         return notifID;
     }
 
-    public Date getNotificationDate() {
-        return notificationDate;
+    public Calendar getNotifDate() {
+        return notifDate;
+    }
+
+    public void setNotifDate(Calendar date) {
+        this.notifDate = date;
+    }
+
+    public void displayNotification() {
+        System.out.println("Notification #" + this.notifID);
+        System.out.println("Date of Notification: " 
+        + notifDate.get(Calendar.MONTH)
+        + notifDate.get(Calendar.DAY_OF_MONTH));
     }
 }
--- MyKitchen/src/NotificationFactory.java
+++ MyKitchen/src/NotificationFactory.java
@@ -1,8 +1,18 @@
+import java.util.Calendar;
+
+enum NotifType {
+    VIEW
+}
+
 public class NotificationFactory {
     NotificationFactory() {
     }
 
-    public Notification createNotification(int id) {
+    public Notification createNotification(int id, Calendar notifDate, NotifType type, String message) {
+        switch (type) {
+            case VIEW:
+                return new ViewNotification(id, notifDate, message);
+        }
         return null;
     }
 }
