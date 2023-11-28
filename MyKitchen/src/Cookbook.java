f71440bb0a9a7b604ab5bfe5e75dd7c24ea85d28
BennyGenny <117399625+BennyGenny@users.noreply.github.com>
2023-11-28 15:22:32 -0600

    cookbook stuff

--- MyKitchen/src/Cookbook.java
+++ MyKitchen/src/Cookbook.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+
 public class Cookbook {
     protected String cookbookName = "";
     //Recipe object containing multiple recipes(recipeName: recipeID: recipeIngredients: recipeSteps)
-    public List<Recipe> recipes = new List<Recipe>();
+    public ArrayList<Recipe> recipes = new ArrayList<Recipe>();
+
+    //function that grabs a recipe from the list of Recipes
+    public Recipe getRecipe(Recipe recipe){
+        
+    }
+
+    //function that removesa recipe from the cookbook
+    public void removeRecipe(Recipe recipe){
+        
+    }
 }
--- MyKitchen/src/CookbookService.java
+++ MyKitchen/src/CookbookService.java
@@ -1,6 +1,12 @@
 public class CookbookService {
-    saveRecipe(newRecipe) {
-        //stores the new recipe object in cookbooks recipes
+    //stores the new recipe object in cookbooks recipes
+    public Recipe saveRecipe(Recipe newRecipe) {
         Cookbook.recipes.add(newRecipe);
+        return newRecipe;
+    }
+
+    //removes the input recipe object from cookbooks recipes
+    public void deleteRecipe(Recipe recipe) {
+        Cookbook.removeRecipe(recipe);
     }
 }
