5e1f5a734d59ceb8e907987bf8270b3d73a414e9
BennyGenny <117399625+BennyGenny@users.noreply.github.com>
2023-11-28 20:35:43 -0600

    Kitchen function skeletons

--- MyKitchen/src/Kitchen.java
+++ MyKitchen/src/Kitchen.java
@@ -1,6 +1,32 @@
+import java.util.Calendar;
+
 public class Kitchen 
 {
     public Pantry inventory ;
     public Pantry shoppingCart;
     public Cookbook myCookbook;
+
+    public Pantry createPantry(String name){
+        return inventory;
+    }
+
+    public Pantry retrievePantry(int pantryID){
+        return inventory;
+    }
+
+    public void addItem(String name, Calendar dateAdded, int quantity){
+
+    }
+
+    public void addItem(String name, Calendar dateAdded, int quantity, Calendar expirDate){
+
+    }
+
+    public void editItem(int itemID, String name, Calendar dateAdded, int quantity, Calendar expirDate){
+
+    }
+
+    public void editItem(int itemID, String name, Calendar dateAdded, int quantity){
+
+    }
 }
