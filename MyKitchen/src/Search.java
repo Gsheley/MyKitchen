a607683b5c5f8208309a99ed2ff7244b06646dd1
DrewPap <146133816+DrewPap@users.noreply.github.com>
2023-11-29 12:08:11 -0600

    Update Search.java

--- MyKitchen/src/Search.java
+++ MyKitchen/src/Search.java
@@ -4,21 +4,34 @@ public class Search
 {
     public static ArrayList<Object> search(String query, ArrayList<Object> list )
     {
-        ArrayList<Object> results = new ArrayList<Object>();
-        for(int i =0; i < list.size();i++)
-       {
-        if(list.get(i).name()==query)
+        //Search case for Item objects 
+        if(list.get(0) instanceof Item)
         {
-
+            ArrayList<Object> results  = new ArrayList<Object>();
+            for(int i =0; i < list.size(); i++)
+            {
+                if(((Item)list.get(i)).getName()==query)
+                {
+                    results.add(list.get(i));
+                }
+            }
+            return  results;
         }
-       }
        
+        //Search case for Recipe objects 
+        if(list.get(0) instanceof Recipe)
+        {
+            ArrayList<Object> results  = new ArrayList<Object>();
+            for(int i =0; i < list.size(); i++)
+            {
+                if(((Recipe)list.get(i)).recipeName==query)
+                {
+                    results.add(list.get(i));
+                }
+            }
+            return  results;
+        }
        
-       
-       
-       
-       
-       
-        return list;
+       return null;
     }
 }
