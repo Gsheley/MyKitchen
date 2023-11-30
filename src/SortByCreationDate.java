1216bf14c997931694246150c52dfee5bf96c6c1
DrewPap <146133816+DrewPap@users.noreply.github.com>
2023-11-30 16:39:30 -0600

    Updated the sort classes again

--- src/SortByCreationDate.java
+++ src/SortByCreationDate.java
@@ -1,16 +1,14 @@
 import java.util.ArrayList;
-import java.util.Calendar;
 
 public class SortByCreationDate implements Sort 
 {
-    public ArrayList<Object> sort(ArrayList<Object> list)
+    public ArrayList<Item> sort(ArrayList<Item> list)
     {
         for (int i = 1; i < list.size(); ++i)
          {
             Item curr = (Item)list.get(i);
-            //int key = ((Item)list.get(i)).getQuantity();
             int j = i - 1;
-            while (j >= 0 && ((Item)list.get(j)).getDateAdded().compareTo(((Item)list.get(i)).getDateAdded()) > 0) 
+            while (j >= 0 && list.get(j).getDateAdded().compareTo(list.get(i).getDateAdded()) > 0) 
             {
                 list.set(j+1, list.get(j));
                 j--;
--- src/SortByName.java
+++ src/SortByName.java
@@ -1,17 +1,15 @@
 import java.util.ArrayList;
-import java.util.Calendar;
 
 public class SortByName implements Sort 
 {
-    public ArrayList<Object> sort(ArrayList<Object> list)
+    public ArrayList<Item> sort(ArrayList<Item> list)
     {
-        //Collections.sort(list, new ItemNameComparator());
         for (int i = 1; i < list.size(); ++i)
          {
-            Item curr = (Item)list.get(i);
-            String key = ((Item)list.get(i)).getName();
+            Item curr = list.get(i);
+            String key = list.get(i).getName();
             int j = i - 1;
-            while (j >= 0 && key.compareTo(((Item)list.get(j)).getName()) < 0) 
+            while (j >= 0 && key.compareTo(list.get(j).getName()) < 0) 
             {
                 list.set(j+1, list.get(j));
                 j--;
