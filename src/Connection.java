673cbb0bf3309490cc15b54f48d7ca1c12fecdd4
Gsheley <131051619+Gsheley@users.noreply.github.com>
2023-12-05 16:11:56 -0600

    Improvements

--- src/Connection.java
+++ src/Connection.java
@@ -1,7 +1,3 @@
 public abstract class Connection {
-    public Connection conn;
     
-    public abstract void save();
-
-    public abstract void delete();
 }
--- src/SaveAppData.java
+++ src/SaveAppData.java
@@ -1,3 +1,7 @@
 public abstract class SaveAppData {
+    public Connection conn;
     
+    public abstract void save();
+
+    public abstract void delete();
 }
