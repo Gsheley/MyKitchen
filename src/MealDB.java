ddde6fa8fa635093639db4081e92f428e60bea88
Chris Blicharz <chblicharz01@gmail.com>
2023-12-05 16:36:56 -0600

    Updated MealDB.java

--- src/MealDB.java
+++ src/MealDB.java
@@ -4,11 +4,12 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import javax.management.RuntimeErrorException;
-
 import com.google.gson.JsonParser;
 
 public class MealDB extends API{
@@ -21,6 +22,11 @@ public class MealDB extends API{
     //query API by recipe name
     public Recipe queryByName(String name)
     {
+        //recipe name is formatted to be readable by the API
+        name.trim();
+        name.replaceAll(" ", "_");
+        name.toLowerCase();
+
         try{
             String queryURL = BASE_URL + "search.php?s="+ name;
             URL url = new URL(queryURL);
@@ -33,7 +39,8 @@ public class MealDB extends API{
 
             if(response_code != 200)
             {
-                throw new RuntimeException("HttpResponseCode: " + response_code);
+                //throw new RuntimeException("HttpResponseCode: " + response_code);
+                return null;
             }
             else{
                 System.out.println("Connection successful with response code: " + response_code);
@@ -66,10 +73,16 @@ public class MealDB extends API{
     }
 
     //query API by main ingredient
-    public String queryByIngredient(String mainIngredient)
+    public ArrayList<Recipe> queryByIngredient(String mainIngredient)
     {
+        ArrayList<Recipe> recipes = new ArrayList<>();
+        //mainIngredient must be formatted to be readable to the database
+        mainIngredient.replaceAll(" ", "_");
+        mainIngredient.trim();
+        mainIngredient.toLowerCase();
+
         try{
-            String queryURL = BASE_URL + "search.php?s="+ "filter.php?i="+ mainIngredient;
+            String queryURL = BASE_URL + "filter.php?i="+ mainIngredient;
             URL url = new URL(queryURL);
             HttpURLConnection url_connection = (HttpURLConnection) url.openConnection();
             url_connection.setRequestMethod("GET");
@@ -98,19 +111,28 @@ public class MealDB extends API{
             while ((line = reader.readLine()) != null) {
                 content.append(line);
             }
-
+            line = content.toString();
             JsonObject jsonObject = JsonParser.parseString(line).getAsJsonObject();
             JsonArray mealsArray = jsonObject.getAsJsonArray("meals");
             StringBuilder mealNames = new StringBuilder();
-
             
+            for (JsonElement mealElement :mealsArray){
+                JsonObject mealObject = mealElement.getAsJsonObject();
+                String mealName = mealObject.get("strMeal").getAsString();
+                Recipe newRecipe = this.queryByName(mealName);
+                if(newRecipe != null){
+                    recipes.add(newRecipe);
+                }
+            }
+
+
             // Close connections
             in.close();
             url_connection.disconnect();
 
             // Convert the JSON string into a Recipe object
-            String return_string = "\n\nThese are the recipes you are able to cook with "+ mainIngredient+": \n";
-            return return_string;
+            //String return_string = "\n\nThese are the recipes you are able to cook with "+ mainIngredient+": \n";
+            return recipes;
         }
         catch (Exception e){
             e.printStackTrace();
@@ -168,15 +190,15 @@ public class MealDB extends API{
     public static void main(String[] args)
     {
         MealDB myConnection = new MealDB();
-        Recipe arrabiata = myConnection.queryByName("Arrabiata");
-        System.out.println("\n\nRecipe by name 'arrabiata': \n\n");
-        arrabiata.printRecipe();
+        Recipe recipeByName = myConnection.queryByName("chicken_ham_and_leek_pie");
+        System.out.println("\n\nRecipe by name 'CHLPie': \n\n");
+        recipeByName.printRecipe();
 
         System.out.println("\n\nRecipe by main ingredient 'chicken_breast': ");
         System.out.println(myConnection.queryByIngredient("chicken_breast"));
         
-        System.out.println("\n\nRecipe by random: \n\n");
-        Recipe random = myConnection.queryRandom();
-        random.printRecipe();
+        //System.out.println("\n\nRecipe by random: \n\n");
+        //Recipe random = myConnection.queryRandom();
+        //random.printRecipe();
     }
 }
