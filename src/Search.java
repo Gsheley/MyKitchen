4af6127dd728659d716e684b8857dc1465dca220
DrewPap <146133816+DrewPap@users.noreply.github.com>
2023-12-05 15:55:57 -0600

    Added test folder and modified search

--- src/Search.java
+++ src/Search.java
@@ -4,34 +4,36 @@ public class Search
 {
     public static ArrayList<Object> search(String query, ArrayList<Object> list )
     {
-        //Search case for Item objects 
-        if(list.get(0) instanceof Item)
+        ArrayList<Object> results = new ArrayList<Object>();
+        if(list.size()>=1)
         {
-            ArrayList<Object> results  = new ArrayList<Object>();
-            for(int i =0; i < list.size(); i++)
+            
+            //Search case for Item objects 
+            if(list.get(0) instanceof Item)
             {
-                if(((Item)list.get(i)).getName().contains(query))
+                for(int i =0; i < list.size(); i++)
                 {
-                    results.add(list.get(i));
+                    if(((Item)list.get(i)).getName().contains(query))
+                    {
+                        results.add(list.get(i));
+                    }
                 }
+                return  results;
             }
-            return  results;
-        }
        
-        //Search case for Recipe objects 
-        if(list.get(0) instanceof Recipe)
-        {
-            ArrayList<Object> results  = new ArrayList<Object>();
-            for(int i =0; i < list.size(); i++)
+            //Search case for Recipe objects 
+            if(list.get(0) instanceof Recipe)
             {
-                if(((Recipe)list.get(i)).recipeName.contains(query))
+                for(int i =0; i < list.size(); i++)
                 {
-                    results.add(list.get(i));
+                    if(((Recipe)list.get(i)).recipeName.contains(query))
+                    {
+                        results.add(list.get(i));
+                    }
                 }
+                return  results;
             }
-            return  results;
         }
-       
-       return null;
+       return results;
     }
 }
