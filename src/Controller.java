3549e72088c89e10cbecfa768a01f1b7906ee5f6
Gsheley <131051619+Gsheley@users.noreply.github.com>
2023-12-05 15:34:53 -0600

    Update Controller

--- src/Controller.java
+++ src/Controller.java
@@ -3,57 +3,60 @@ import java.util.Calendar;
 public class Controller {
     static Kitchen kitchen = new Kitchen();
     NotificationService ns = new NotificationService();
-    int numKitchens = 0;
-    int numCarts = 0;
+    static int numKitchens = 0;
+    static int numCarts = 0;
     public static void main(String[] args) 
     {
         Navigation nv = new Navigation();
         nv.printHomePage();
     }
 
-    public void deletePantry(int id) 
+    public static void deletePantry(int id) 
     {
-        kitchen.deletePantry(id);
+        Kitchen.deletePantry(id);
         numKitchens--;
     }
 
-    public void createPantry() 
+    public static void createPantry() 
     {
-        kitchen.createPantry(PantryType.KITCHEN_INVENTORY,Navigation.getUserInputString(false));
+        Kitchen.createPantry(PantryType.KITCHEN_INVENTORY, Navigation.getUserInputString(true));
         numKitchens++;
     }
 
-    public void createCart() 
+    public static void createCart() 
     {
-        // Call to shared scanner class to ge the name of the new shopping cart
-        kitchen.createPantry(PantryType.SHOPPING_CART, Navigation.getUserInputString(false));
+        Kitchen.createPantry(PantryType.SHOPPING_CART, Navigation.getUserInputString(true));
         numCarts++;
     }
 
-    public void deleteCart(int id) 
+    public static void deleteCart(int id) 
     {
-        kitchen.deletePantry(id);
+        Kitchen.deletePantry(id);
         numCarts--;
     }
 
-    public void addItem(String name, int quantity, Calendar expirationDate) 
+    public static void addItem(int pantryID) 
     {
-
+        System.out.println("Please enter the name of the Item: ");
+        String name = Navigation.getUserInputString(true);
+        System.out.println("Please enter the quantity of the Item: ");
+        int quantity = Navigation.getUserInputInt(1, Integer.MAX_VALUE);
+        Kitchen.addItem()
     }
 
-    public int getNumKitchens() {
+    public static int getNumKitchens() {
         return numKitchens;
     }
 
-    public int getNumCarts() {
+    public static int getNumCarts() {
         return numCarts;
     }
 
-    public void setNumKitchens(int num) {
+    public static void setNumKitchens(int num) {
         numKitchens = num;
     }
 
-    public void setNumCarts(int num) {
+    public static void setNumCarts(int num) {
         numCarts = num;
     }
 }
