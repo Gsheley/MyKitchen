8bbd4c89fbbe9022f190426618eb3952e3e25512
DrewPap <146133816+DrewPap@users.noreply.github.com>
2023-12-07 15:38:49 -0600

    Added kitchenTest

---
+++ src/KitchenTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertArrayEquals;
+import java.util.ArrayList;
+import org.junit.Before;
+import org.junit.Test;
+
+public class KitchenTest 
+{
+    
+}
+    
--- src/PantryService/Test.java
+++
@@ -1,5 +0,0 @@
-package PantryService;
-
-public class Test {
-
-}
