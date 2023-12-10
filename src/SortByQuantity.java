32cdaa59bbe29081bfa46377899f63e5b892bbd4
Gsheley <131051619+Gsheley@users.noreply.github.com>
2023-12-10 15:53:57 -0600

    Removing a Test Main Function

--- src/SortByQuantity.java
+++ src/SortByQuantity.java
@@ -21,28 +21,4 @@ public class SortByQuantity implements Sort
         }
         return list;
     }   
-
-    public static void main(String[] args) 
-    {
-        ArrayList<Item> list = new ArrayList<>();
-        String[] names = {"apple", "bannana", "pear", "lettuce", "chips"};
-        Item item1 = new Item(5, names[0], null, 12);
-        Item item2 = new Item(4, names[1], null, 9);
-        Item item3 = new Item(3, names[2], null, 6);
-        Item item4 = new Item(2, names[3], null, 4);
-        Item item5 = new Item(1, names[4], null, 2212);
-        list.add(item1);
-        list.add(item2);
-        list.add(item3);
-        list.add(item4);
-        list.add(item5);
-
-        SortByQuantity sorter = new SortByQuantity();
-        ArrayList<Item> result = sorter.sort(list);
-
-        for(int i =0; i < result.size(); i++)
-        {
-            System.out.println(result.get(i).getName());
-        }
-    }
 }
